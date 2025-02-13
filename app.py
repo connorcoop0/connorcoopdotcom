3c2eb2da2c447ec53fb8221a11b6606e71786056
Ubuntu <ubuntu@ip-172-31-81-76.ec2.internal>
2025-02-13 06:07:54 +0000

    Initial commit

---
+++ app.py
@@ -0,0 +1,30 @@
+import re
+from datetime import datetime
+
+from flask import Flask
+from flask import render_template
+
+
+app = Flask(__name__)
+
+@app.route("/")
+def home():
+    return render_template("home.html")
+
+@app.route("/contact/")
+def contact():
+    return render_template("contact.html")
+
+@app.route("/projects/")
+def projects():
+    return render_template("projects.html")
+
+@app.route("/hello/")
+@app.route("/hello/<name>")
+def hello_there(name = "user"):
+    return render_template(
+        "hello_there.html",
+        name=name,
+        date=datetime.now()
+    )
+
