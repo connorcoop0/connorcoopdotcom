9fdea85d14c797d562e271d32ff897186828be5d
connorcoop0 <126022520+connorcoop0@users.noreply.github.com>
2025-02-18 11:18:07 -0500

    Removed practice page + minor changes
    - Removed the practice page from layout.html and app.py.
    
    + added tab title to hello page
    
    +prettier reformatting

--- app.py
+++ app.py
@@ -19,10 +19,6 @@ def contact():
 def projects():
     return render_template("projects.html")
 
-@app.route("/practice/")
-def practice():
-    return render_template("practice.html")
-
 @app.route("/hello/")
 @app.route("/hello/<name>")
 def hello_there(name = "user"):
