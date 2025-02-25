acbd17803284d4113e5d5987c848ea76205b15f4
connorcoop0 <126022520+connorcoop0@users.noreply.github.com>
2025-02-25 12:38:28 -0500

    Updated Education, Skills, Projects, and Experience sections
    Updated all the sections with extra info and got everything to a point where it is presentable

--- app.py
+++ app.py
@@ -10,21 +10,3 @@ app = Flask(__name__)
 @app.route("/")
 def home():
     return render_template("index.html")
-
-@app.route("/contact/")
-def contact():
-    return render_template("contact.html")
-
-@app.route("/projects/")
-def projects():
-    return render_template("projects.html")
-
-@app.route("/hello/")
-@app.route("/hello/<name>")
-def hello_there(name = "user"):
-    return render_template(
-        "hello_there.html",
-        name=name,
-        date=datetime.now()
-    )
-
